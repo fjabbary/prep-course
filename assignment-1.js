519d225cf14d2807c99aea1bbc617734608eb658
Farzin <farzin.jabbary@gmail.com>
2018-10-21 12:24:27 -0400

    Basic Javascript

---
+++ assignment-1.js
@@ -0,0 +1,15 @@
+let numbers = [];
+
+for (let i = 0; i < 10; i++) {
+  numbers.push(i);
+}
+
+//logs the first item of the array
+console.log(numbers[0])
+
+//logs the last item of the array: numbers[9]
+console.log(numbers[numbers.length - 1])
+
+let car = {
+  colour: "black"
+};
